0d7e86f9d9bfbe0dda772df48cdc05545c8ee28a
Jongwhan Lee <51560997+leejw51crypto@users.noreply.github.com>
2020-03-17 09:40:04 +0800

    Problem: (cro-1231) no tx-building in ios example using c-bindings (#2)
    Solution: add low level tx-building api (unbond, withdraw, transfer, deposit)
    clear data

--- IosSample/IosSample/IosSample-Bridging-Header.h
+++ IosSample/IosSample/IosSample-Bridging-Header.h
@@ -1,10 +1,12 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
 #include "chain-core.h"
 #include "chain.h"
+// json rpc
 void restore_wallet(const char* tendermint_url, const char* storage,  const char* name, const char* passphrase, const char* enckey, const char* mnemonics);
 void sync_wallet(const char* tendermint_url, const char* storage,  const char* name, const char* passphrase, const char* enckey, const char* mnemonics);
 float get_rate();
 void stop_sync();
+// lowlevel
+uint64_t get_nonce_user(const char* tendermint_url, CroAddressPtr staking);
+void test_number(char* buf, int* number);
+void withdraw_by_address(const char* tendermint_url,unsigned char network_id,CroAddressPtr staking, const char* to_addr, const char* viewkey, unsigned char* buf, unsigned int* buf_len);
+const  char* get_address_user(CroAddressPtr addr);
